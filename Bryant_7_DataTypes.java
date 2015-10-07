f338c37725f633d7414e29120cab7767143729d8
Max Bryant <Mbryant2078@gmail.com>
2015-10-07 10:37:07 -0500

    Data Types

---
+++ Bryant_7_DataTypes.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bryant_7_datatypes;
+
+/**
+ *
+ * @author Max_and_a_Mango
+ */
+public class Bryant_7_DataTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //space for comments
+        //used to explain code
+        //code explanation
+        //in D22
+        int i = 1;
+        float a = 2.5f;
+        double b = 3.87;
+        short c = 16;
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4 + "";
+        String e1 = "Amanda is the Best";
+        boolean f = true;
+        boolean g = i % 2 == 0;
+        /*
+         here we will look at some mathmatical operators
+         */
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers or more
+        // / this divides two numbers or more
+        // % this is modulo. Shows you the remainder
+        System.out.println(i + " an integer");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(e + " a string");
+        System.out.println(f + " a boolean");
+        for (i = -123; i < 123; i++) {
+            //System.out.println((char) i);
+
+            System.out.println(i % 3 == 0);
+        }
+        /* % checks for the remainder
+         i % 2 checks if a number is even or odd
+         */
+
+    // a number value for char
+        // maps to a character
+//   i = 22;
+//   a = 0;
+//   e = "ninja";
+        //System.out.println(e + i + " an /" + "a string plus an integer");   
+        // upcast to float is automatic
+        // i.e. 7 + 0.0 = 7.0 i = 22;
+ 
+            /*if is a conditional (flow) statement. If a condition is true the following
+             command will be executed
+             */
+}
+}
+
